8d12cb441a2f90ecbef587f886b2f193e04dac9a
John Gildred <274078+jgildred@users.noreply.github.com>
2024-07-07 21:56:36 -0700

    update build script

--- build-frontend.sh
+++ build-frontend.sh
@@ -2,4 +2,4 @@ cd makepad-site-frontend
 cargo makepad wasm build -p makepad-site-frontend --release
 cd ..
 rm -rf webroot
-mv -f frontend/target/makepad-wasm-app/release/makepad-site-frontend webroot
+mv -f makepad-site-frontend/target/makepad-wasm-app/release/makepad-site-frontend webroot
