b05eb6d92650c607d72d0e91a8bede2e33f232d4
John Gildred <274078+jgildred@users.noreply.github.com>
2024-07-29 20:11:51 -0700

    minor update

--- build-frontend.sh
+++ build-frontend.sh
@@ -3,3 +3,5 @@ cargo makepad wasm build -p makepad-site-frontend --release
 cd ..
 rm -rf webroot
 mv -f makepad-site-frontend/target/makepad-wasm-app/release/makepad-site-frontend webroot
+cp -rf assets webroot
+cp -rf data webroot
